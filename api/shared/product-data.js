1cbec8f7dafa59569f3d2a754de891d595cc06c3
Rino Reji Cheriyan <mailrino@gmail.com>
2022-12-05 13:47:28 +0000

    cleanup

--- api/shared/product-data.js
+++ api/shared/product-data.js
@@ -18,6 +18,12 @@ const data = {
       description: 'Bag of 7 fresh McIntosh apples',
       quantity: 1,
     },
+    {
+      id: 40,
+      name: 'Milk',
+      description: 'Milma',
+      quantity: 1,
+    },
   ],
 };
 
